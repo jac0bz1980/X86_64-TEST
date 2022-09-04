6b2ee4ef546b4e11f83321cd697a98bfa716b423
jac0bz1980 <82744535+jac0bz1980@users.noreply.github.com>
2022-09-04 10:49:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -49,3 +49,4 @@ git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-ipt
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/sbwml/luci-app-alist package/alist
